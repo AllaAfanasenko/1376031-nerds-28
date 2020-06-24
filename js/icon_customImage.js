1e18ac498e77ae95e8c9216395a38b579441455c
AllaAfanasenko <alla.afanasenko@gmail.com>
2020-06-24 15:45:37 +0300

    Issues fixed according to comments

---
+++ js/icon_customImage.js
@@ -0,0 +1,37 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.939113, 30.321420],
+            zoom: 17
+        });
+        myMap.controls.remove('geolocationControl');
+        myMap.controls.remove('searchControl');
+        myMap.controls.remove('trafficControl');
+        myMap.controls.remove('typeSelector');
+        myMap.controls.remove('fullscreenControl');
+        myMap.controls.remove('rulerControl');
+        myMap.controls.remove('zoomControl');
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'NЁRDS DESIGN STUDIO, 191186, Санкт-Петербург, ул. Б. Конюшенная, д. 19/8'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/footer/mapMarker.png',
+            // Размеры метки.
+            iconImageSize: [231, 190],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [110, -125]
+        });
+
+
+    myMap.geoObjects
+        .add(myPlacemark);
+});
